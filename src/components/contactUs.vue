0e02cfb5452324d65c99c2377ffd8935ed65d945
Magnus Arsaelsson <magnus.thor.ar@gmail.com>
2022-04-08 19:02:52 +0200

    adds form

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,9 +1,44 @@
 <template>
   <div>Contact us area</div>
+  <div class="form">
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name:
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email:
+          <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message:
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 </template>
 <script>
 export default {
   name: "ContactUs",
 };
 </script>
-<style lang="sass"></style>
+<style lang="scss">
+.form {
+  border: 1px solid;
+  width: 25rem;
+  margin: 2rem;
+
+  p {
+    margin: 1rem;
+  }
+}
+</style>
