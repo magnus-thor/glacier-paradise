67a093b3700a887c1c91e1e0b546dddfff0fbcb3
Magnus Thor <magnus.thor.ar@gmail.com>
2023-03-14 19:28:44 +0100

    WIP styling midnightsun

--- src/components/tourComponents/midnight-sun-tour.vue
+++ src/components/tourComponents/midnight-sun-tour.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="midnightSun--wrapper">
+    <img src="/images/midnightsun-630-1200.sida.jpg" alt="" />
+
     <h1>{{ $t("midnightSun.header") }}</h1>
-    <img src="" alt="TODO fix" />
-    <p>{{ $t("midnightSun.p1") }}</p>
-    <p>{{ $t("midnightSun.p2") }}</p>
-    <p>{{ $t("midnightSun.p3") }}</p>
-    <p>{{ $t("midnightSun.p4") }}</p>
+    <div class="p1">
+      <p>{{ $t("midnightSun.p1") }}</p>
+      <img src="/images/bjarturhiminn.jpg" alt="" />
+    </div>
+    <div class="p2">
+      <img src="/images/rutan.jpg" alt="" />
+      <p>{{ $t("midnightSun.p2") }}</p>
+    </div>
+    <div class="p3">
+      <p>{{ $t("midnightSun.p3") }}</p>
+      <img src="/images/trodararnir.jpg" alt="" />
+    </div>
+    <div class="p4">
+      <img src="/images/midnightsun.card.jpg" alt="" />
+      <p>{{ $t("midnightSun.p4") }}</p>
+    </div>
   </div>
 </template>
 
@@ -19,16 +32,37 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/base.scss";
+@import "@/assets/mixins.scss";
+
 .midnightSun--wrapper {
   margin: auto;
-  max-width: 100ch;
+  @include yellow-brownish-bg-gradient;
 
   h1 {
-    text-align: center;
+    padding-left: 2rem;
+    color: $white;
+    // margin-bottom: 1rem;
   }
 
-  p {
-    margin-top: 1rem;
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  .p1,
+  .p2,
+  .p3,
+  .p4 {
+    padding: 0 2rem;
+    // display: flex;
+    align-items: center;
+
+    img {
+      width: 180px;
+      height: auto;
+      border-radius: 50%;
+    }
   }
 }
 </style>
