348a52054e5c5e7d72f5bbee8aa68903782b3d9f
Magnus Thor <magnus.thor.ar@gmail.com>
2023-03-28 11:13:07 +0200

    split locale files into smaller pieces

---
+++ src/locales/en/index.js
@@ -0,0 +1,19 @@
+import home from "./home.json";
+import aboutUs from "./about-us.json";
+import contactUs from "./contact-us.json";
+import midnightSun from "./midnight-sun.json";
+import topOfTheDiamond from "./top-of-the-diamond.json";
+import cards from "./tour-cards.json";
+import shared from "./shared.json";
+import header from "./header.json";
+
+export default {
+  home,
+  aboutUs,
+  contactUs,
+  midnightSun,
+  topOfTheDiamond,
+  cards,
+  shared,
+  header,
+};
